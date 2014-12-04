119c0159efbe746efbb220ed6541e9ff611e41fd
MrJMO <jmolayao@gmail.com>
2014-12-04 10:45:37 -0500

    Normalización bien hecha. Basura de John y corrección bien hecha por Luisfe y JuanMa

--- NormalizacionListaEmpresas/src/normalizacionlistaempresas/Lector.java
+++ NormalizacionListaEmpresas/src/normalizacionlistaempresas/Lector.java
@@ -1,6 +1,9 @@
 package normalizacionlistaempresas;
 
 import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
@@ -22,7 +25,7 @@ public class Lector {
         try {
             // Apertura del fichero y creacion de BufferedReader para poder
             // hacer una lectura comoda (disponer del metodo readLine()).
-            archivo = new File("C:\\Users\\Estefany\\Desktop\\John\\clusteringKdd\\Listado Empresas - Editado v2.txt");
+            archivo = new File("/home/juan/GitProjects/clusteringKdd/Listado Empresas - Editado v2.txt");
             fileReader = new FileReader(archivo);
             bufferedReader = new BufferedReader(fileReader);
 
@@ -109,7 +112,7 @@ public class Lector {
         }
 
         for (int j = 0; j < 7; j++) {
-            media[j] = ((float) 1 / n) * sumatoria[j];
+            media[j] = (float) sumatoria[j] / n;
         }
 
         numeroRegistros = n;
@@ -123,17 +126,24 @@ public class Lector {
 
         for (int i = 0; i < empresas.size(); i++) {
 
-            desviacionEstandar[0] += ((float) 1 / numeroRegistros) * (empresas.get(i).getIngresosOperacionales() - media[0]);
+            desviacionEstandar[0] += Math.pow((empresas.get(i).getIngresosOperacionales() - media[0]), 2);
 //            String split[] = empresas.get(i).getVariacionIngresos().split("%");
 //            float valor = Float.parseFloat(split[0]);
 //            float porcentaje = valor / 100;
 //            desviacionEstandar[1] += ((float) 1 / numeroRegistros) * (porcentaje - media[1]);
-            desviacionEstandar[2] += ((float) 1 / numeroRegistros) * (empresas.get(i).getUtilidadOperacional() - media[2]);
-            desviacionEstandar[3] += ((float) 1 / numeroRegistros) * (empresas.get(i).getUtilidadNeta() - media[3]);
-            desviacionEstandar[4] += ((float) 1 / numeroRegistros) * (empresas.get(i).getActivoTotal() - media[4]);
-            desviacionEstandar[5] += ((float) 1 / numeroRegistros) * (empresas.get(i).getPasivoTotal() - media[5]);
-            desviacionEstandar[6] += ((float) 1 / numeroRegistros) * (empresas.get(i).getPatrimonioTotal() - media[6]);
+            desviacionEstandar[2] += Math.pow((empresas.get(i).getUtilidadOperacional() - media[2]), 2);
+            desviacionEstandar[3] += Math.pow((empresas.get(i).getUtilidadNeta() - media[3]), 2);
+            desviacionEstandar[4] += Math.pow((empresas.get(i).getActivoTotal() - media[4]), 2);
+            desviacionEstandar[5] += Math.pow((empresas.get(i).getPasivoTotal() - media[5]), 2);
+            desviacionEstandar[6] += Math.pow((empresas.get(i).getPatrimonioTotal() - media[6]), 2);
         }
+
+        desviacionEstandar[0] = (float) Math.sqrt((desviacionEstandar[0] / numeroRegistros));
+        desviacionEstandar[2] = (float) Math.sqrt((desviacionEstandar[2] / numeroRegistros));
+        desviacionEstandar[3] = (float) Math.sqrt((desviacionEstandar[3] / numeroRegistros));
+        desviacionEstandar[4] = (float) Math.sqrt((desviacionEstandar[4] / numeroRegistros));
+        desviacionEstandar[5] = (float) Math.sqrt((desviacionEstandar[5] / numeroRegistros));
+        desviacionEstandar[6] = (float) Math.sqrt((desviacionEstandar[6] / numeroRegistros));
 
         return desviacionEstandar;
     }
@@ -145,19 +155,24 @@ public class Lector {
 
         try {
             //Abro stream, crea el fichero si no existe
-            fileWriter = new FileWriter("C:\\Users\\Estefany\\Desktop\\John\\clusteringKdd\\Listado Empresas - Normalizado.txt");
+            fileWriter = new FileWriter("/home/juan/GitProjects/clusteringKdd/Listado Empresas - Normalizado.txt");
             pw = new PrintWriter(fileWriter);
 
             for (Empresa empresa1 : empresas) {
 
-                DecimalFormat df = new DecimalFormat("#");
-                df.setMaximumFractionDigits(8);
-                //System.out.println(df.format(myvalue));
+                //Conversion de expresión científica a decimal de 10 posiciones 
+                String ingresosOperacionales = new BigDecimal(empresa1.getIngresosOperacionales(), new MathContext(10)).toPlainString();
+                String utilidadOperacional = new BigDecimal(empresa1.getUtilidadOperacional(), new MathContext(10)).toPlainString();
+                String utilidadNeta = new BigDecimal(empresa1.getUtilidadNeta(), new MathContext(10)).toPlainString();
+                String activoTotal = new BigDecimal(empresa1.getActivoTotal(), new MathContext(10)).toPlainString();
+                String pasivoTotal = new BigDecimal(empresa1.getPasivoTotal(), new MathContext(10)).toPlainString();
+                String patrimonioTotal = new BigDecimal(empresa1.getPatrimonioTotal(), new MathContext(10)).toPlainString();
 
-                pw.println("" + df.format(empresa1.getIngresosOperacionales()) + " " + empresa1.getVariacionIngresos()
-                        + " " + df.format(empresa1.getUtilidadOperacional()) + " " + df.format(empresa1.getUtilidadNeta())
-                        + " " + df.format(empresa1.getActivoTotal()) + " " + df.format(empresa1.getPasivoTotal())
-                        + " " + df.format(empresa1.getPatrimonioTotal()));
+                pw.println("" + ingresosOperacionales + " " + empresa1.getVariacionIngresos()
+                        + " " + utilidadOperacional + " " + utilidadNeta
+                        + " " + activoTotal + " " + pasivoTotal
+                        + " " + patrimonioTotal);
+                
             }
 
         } catch (Exception e) {
